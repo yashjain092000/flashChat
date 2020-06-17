976784de27ef9d2846f666f48fbf5dcc5d7aafaf
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-06-17 15:39:43 -0500

    create 3 properties in that extracted widget named title,colour and onPressed. Make it's constructor and use it wherever required as per desired parameters.

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -66,26 +66,14 @@ class _WelcomeScreenState extends State<WelcomeScreen>  with SingleTickerProvide
             SizedBox(
               height: 48.0,
             ),
-            RoundedButton(),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                    //Go to registration screen.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
-            ),
+            RoundedButton(title:'Log in',colour:Colors.lightBlueAccent,onPressed:() {
+              Navigator.pushNamed(context, LoginScreen.id);
+              //Go to login screen.
+            },),
+          RoundedButton(onPressed:() {
+            Navigator.pushNamed(context, RegistrationScreen.id);
+            //Go to registration screen.
+          },title:'Register',colour:Colors.blueAccent)
           ],
         ),
       ),
@@ -94,9 +82,11 @@ class _WelcomeScreenState extends State<WelcomeScreen>  with SingleTickerProvide
 }
 
 class RoundedButton extends StatelessWidget {
-  const RoundedButton({
-    Key key,
-  }) : super(key: key);
+  RoundedButton({this.title,this.colour, @required this.onPressed});
+
+  final String title;
+  final Color colour;
+  final Function onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -104,17 +94,14 @@ class RoundedButton extends StatelessWidget {
       padding: EdgeInsets.symmetric(vertical: 16.0),
       child: Material(
         elevation: 5.0,
-        color: Colors.lightBlueAccent,
+        color: colour,//Colors.lightBlueAccent,
         borderRadius: BorderRadius.circular(30.0),
         child: MaterialButton(
-          onPressed: () {
-            Navigator.pushNamed(context, LoginScreen.id);
-            //Go to login screen.
-          },
+          onPressed: onPressed,
           minWidth: 200.0,
           height: 42.0,
           child: Text(
-            'Log In',
+            title,
           ),
         ),
       ),
