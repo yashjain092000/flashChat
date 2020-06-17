9b5097e1c89097f6292229bdc1114c093b4f6ada
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-06-17 16:00:34 -0500

    cut the inputDecoration from registration screen and add as a new const to constants file.

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flashChat/components/rounded_button.dart';
+import 'package:flashChat/constants.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id='registration_screen';
@@ -32,22 +33,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               onChanged: (value) {
                 //Do something with the user input.
               },
-              decoration: InputDecoration(
-                hintText: 'Enter your email',
-                contentPadding:
-                EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
+              decoration: kTextFieldDecoration,
             ),
             SizedBox(
               height: 8.0,
